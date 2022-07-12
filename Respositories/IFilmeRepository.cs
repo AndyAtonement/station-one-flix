98e538e17905deee1a7263d69df49c9324c54c4e
Andy <andyatonement@hotmail.com>
2022-07-12 03:51:53 -0300

    Adding the Repositories
    Implemented the Respositories

--- Respositories/IFilmeRepository.cs
+++ Respositories/IFilmeRepository.cs
@@ -1,7 +1,13 @@
+using StationOneFlix.Models;
+
 namespace StationOneFlix.Respositories
 {
     public interface IFilmeRepository
     {
-         
+        Task<Filme> GetById(long id);
+        Task<IEnumerable<Filme>> GetAll();
+        Task<Filme> Create(Filme filme);
+        Task<Filme> Update(Filme filme);
+        Task<Filme> Delete(long id);
     }
 }
